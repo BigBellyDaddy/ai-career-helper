536c9911a24d2cd0b0a68d660ba7bee89c3fb991
BigBellyDaddy <romereez@gmail.com>
2025-12-15 18:49:54 +0100

    [UDPATE] 0.0.4

--- frontend/src/loginPage.jsx
+++ frontend/src/loginPage.jsx
@@ -2,18 +2,82 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
 import logo from "./assets/logo.png";
+import {
+  Button,
+  Box,
+  Typography,
+  FormControl,
+  InputLabel,
+  InputAdornment,
+  OutlinedInput,
+  IconButton,
+  Input,
+  FormHelperText,
+} from "@mui/material";
+
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
 export default function LoginPage() {
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
 
   return (
-    <div>
-      <img src={logo} alt="logo" style={{ height: 400, width: 400 }} />
-      <h1 style={{ color: "black" }}>Welcome Back</h1>
-      <p style={{ color: "black" }}>Log in to unlock your potential.</p>
-      <div jus>
-        <input type="email" placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
-      </div>
-    </div>
+    <Box>
+      <Box
+        component="img"
+        src={logo}
+        alt="logo"
+        style={{ height: 400, width: 400 }}
+      />
+      <Typography color="black" variant="h3" fontWeight={700}>
+        Welcome Back!
+      </Typography>
+      <Typography color="black" variant="subtitle1" fontWeight={700}>
+        Log in to unlock your potential.
+      </Typography>
+      <Box sx={{flexDirection: 'column', display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel>Email</InputLabel>
+          <OutlinedInput
+            endAdornment={<InputAdornment position="end" />}
+            label="Email"
+          />
+        </FormControl>
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel>Password</InputLabel>
+          <OutlinedInput
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? "hide the password" : "display the password"
+                  }
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  onMouseUp={handleMouseUpPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <Button variant="contained" onClick = {() => navigate("/Chat")}>Log In</Button>
+      </Box>
+    </Box>
   );
 }
--- frontend/src/welcomePage.jsx
+++ frontend/src/welcomePage.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css"; 
 import logo from "./assets/logo.png"
+import { Button,Box,Typography } from '@mui/material';
+
 export default function WelcomePage() {
 const navigate = useNavigate();
 
     return (
-    <div >
-      <img src={logo} alt="logo" style={{height: 600 , width: 600}}/>
-      <h1 style={{color: "black"}}>AI CAREER HELPER</h1>
-      <p style={{color: "black"}}>Unlock Your Potential. Guide Your Future.</p>
-      <button onClick = {() => navigate("/LoginPage")} >GET STARTED</button>
-    </div>
+    <Box>
+      <Box component="img" src={logo} alt="logo" sx={{width: 600, height: 600}}/>
+      <Typography color="black" variant="h3" fontWeight={700}>
+        AI CAREER HELPER
+      </Typography>
+
+      <Typography variant="subtitle1" color="text.secondary">
+        Unlock Your Potential. Guide Your Future.
+      </Typography>
+      <Button variant="contained" onClick = {() => navigate("/loginPage")} > GET STARTED</Button>
+    </Box>
   );
 }
